c7583a07ca7afce7f4bc30fcf4b5e267f8cddeb6
reileyvince <nova.vince05@gmail.com>
2024-11-08 14:03:12 +0000

    3. Conditional statements and Logical Operators

---
+++ grades.py
@@ -0,0 +1,13 @@
+mark = int(input("Please input the students mark "))
+if mark < 101 and mark > 69:
+    print("The student got an A")
+if mark < 70 and mark > 59:
+    print("The student got a B")
+if mark < 60 and mark > 49:
+    print("The student got a C")
+if mark < 50 and mark > 39:
+    print("The student got a D")
+if mark < 40 and mark > 29:
+    print("The student got an E")
+if mark < 30 and mark > 19:
+    print("The student got a F")
