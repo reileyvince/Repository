7d9002b6e2845face2253063b92cedd072b5840b
reileyvince <nova.vince05@gmail.com>
2024-11-08 14:02:42 +0000

    2. Variables and IO

---
+++ calc.py
@@ -0,0 +1,7 @@
+first = int(input("Please input your first number "))
+second = int(input("Now enter your second number "))
+
+print(first + second)
+print(first - second)
+print(first * second)
+print(first / second)
